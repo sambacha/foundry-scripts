6d94d3e7d88bdb85f69253d1f3887adf3ecb8d04
sam bacha <sam@manifoldfinance.com>
2022-03-17 01:15:55 -0700

    Create Interface_EventLog.sol

---
+++ src/Interface_EventLog.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+interface <% $Interface %> {
+
+    event log(string);
+    event log_address(address);
+    event log_bytes(bytes);
+    event log_bytes32(bytes32);
+    event log_int(int256);
+    event log_named_address(string key, address val);
+    event log_named_bytes(string key, bytes val);
+    event log_named_bytes32(string key, bytes32 val);
+    event log_named_decimal_int(string key, int256 val, uint256 decimals);
+    event log_named_decimal_uint(string key, uint256 val, uint256 decimals);
+    event log_named_int(string key, int256 val);
+    event log_named_string(string key, string val);
+    event log_named_uint(string key, uint256 val);
+    event log_string(string);
+    event log_uint(uint256);
+    event logs(bytes);
+
+    function IS_TEST() external view returns (bool);
+
+    function failed() external view returns (bool);
+
+    function setUp() external;
+
+    function testExample() external;
+
+    function testForInterface() external;
+
+    receive() external payable;
+}
