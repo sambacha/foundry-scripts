2d9980f622273090d3f8184f0e68960f844f2387
sam bacha <sam@manifoldfinance.com>
2022-05-09 04:12:10 -0700

    build(git): submodule config

--- scripts/git/submodules.sh
+++ scripts/git/submodules.sh
@@ -1,2 +1,19 @@
-echo "Updating and init submodules"
-git submodule sync --recursive && git submodule update --init --recursive
+#!/usr/bin/env bash
+set -o errexit
+
+git --version 
+
+echo "[CONFIG]: Updating and init submodules"
+git config --global diff.submodule log
+git config --global status.submoduleSummary true
+git config --global fetch.recurseSubmodules true
+sleep 1
+
+echo "[TASK]: Syncing submodules"
+git submodule sync --recursive && git submodule update --init --recursive || exit 127
+sleep 1
+
+git submodule foreach git reset --hard ^HEAD
+git submodule update --remote --rebase lib/
+git status
+exit 0
