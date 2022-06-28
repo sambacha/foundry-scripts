74fe65c8325835841d13d448207dff895e59ab35
sam bacha <sam@manifoldfinance.com>
2022-06-27 17:57:21 -0700

    feat: better submodule syncing

--- scripts/git/submodules.sh
+++ scripts/git/submodules.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+
+for branch in $fetch; do
+  git fetch origin $branch
+  git branch $branch FETCH_HEAD
+done
+
+if [ "$ref" == "HEAD" ]; then
+  return_ref=$(git rev-parse HEAD)
+else
+  return_ref=$ref
+fi
+
+
 git --version
 
 echo "[CONFIG]: Updating and init submodules"
@@ -9,11 +22,23 @@ git config --global status.submoduleSummary true
 git config --global fetch.recurseSubmodules true
 sleep 1
 
+# 
+# Git Submodules syncing
+#
+#   Alternative oneliner
+#
+# ```shell
+# git submodule status | awk '{print $2}' | xargs -P5 -n1 git submodule update --init $depthflag --recursive
+#```
+#
+
 echo "[TASK]: Syncing submodules"
-git submodule sync --recursive && git submodule update --init --recursive
+git submodule sync --recursive && git submodule update --init --recursive --jobs=8
 sleep 1
 
-git submodule foreach git reset --hard HEAD
-git submodule update --remote --rebase lib/
+
+
+git submodule foreach git reset --hard HEAD --jobs=8
+git submodule update --remote --rebase lib/ --jobs=8
 git status
 exit 0
