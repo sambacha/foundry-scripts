5980f9318807ea3fb99d4893fbf210d5504e237e
sam bacha <sam@manifoldfinance.com>
2022-07-26 23:57:41 -0700

    Update coverage-snapshot.sh

--- gas-snapshot/src/reporting/coverage-snapshot.sh
+++ gas-snapshot/src/reporting/coverage-snapshot.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+patdiff -version || exit 127
 forge snapshot --allow-failures >02_gas_report
-01_gas_report=$(cat .gas-snapshot)
-patdiff 01_gas_report 02_gas_report -html >gas_report.html
+patdiff .gas-snapshot 02_gas_report -html >gas_report.html
+rm 02_gas_report
+echo "Gas Report generated"
