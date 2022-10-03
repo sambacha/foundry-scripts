3588ffd6ab851ae6e8d66cda62f50200d3f3744a
s bacha <sam@manifoldfinance.com>
2022-10-03 09:08:04 -0700

    fix(codehash): improve bash func

--- scripts/codehash.sh
+++ scripts/codehash.sh
@@ -1,1 +1,14 @@
-cat out/$CONTRACT.sol/$CONTRACT_ABI.json | jq -r .bytecode.object | xargs cast keccak
+#!/usr/bin/env bash
+set -eo pipefail
+
+codehash() {
+if [[ -z $CONTRACT ]]; then
+  if [[ -z ${1} ]]; then
+    echo '"$CONTRACT" env variable is not set. Set it to the name of the contract you want the codehash for.'
+    exit 127
+  else
+    CONTRACT=${1}
+    cat out/$CONTRACT.sol/$CONTRACT.json | jq -r .bytecode.object | xargs cast keccak
+  fi
+fi
+}
